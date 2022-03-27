af33dd1ccfae3927ff0aaea93b4c4467340dfe37
Iván Décima <ivandecima.facultad@gmail.com>
2022-03-27 14:31:26 -0300

    Task List (Incomplete)

---
+++ src/app/mock-task.ts
@@ -0,0 +1,40 @@
+import {Task} from './Task'
+
+export const TASKS:Task[] = [
+    {
+        id: 1,
+        text: "Primera",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Segunda",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "Tercera",
+        day: "Agosto 5 a las 12:00",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Cuarta",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 5,
+        text: "Quinta",
+        day: "Agosto 5 a las 12:00",
+        reminder: true
+    },
+    {
+        id: 6,
+        text: "Sexta",
+        day: "Agosto 5 a las 12:00",
+        reminder: false
+    }
+]
